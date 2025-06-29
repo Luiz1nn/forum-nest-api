6047ae0109962ddbca00807ce8ec5afb5baf5cf8
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-29 12:37:31 -0300

    ✨ feat: add EditQuestion controller and corresponding end-to-end tests for editing questions, including attachment management and validation

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,6 @@
 import { AuthenticateStudentUseCase } from '~/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '~/domain/forum/application/use-cases/create-question'
+import { EditQuestionUseCase } from '~/domain/forum/application/use-cases/edit-question'
 import { RegisterStudentUseCase } from '~/domain/forum/application/use-cases/register-student'
 
 import { Module } from '@nestjs/common'
@@ -9,6 +10,7 @@ import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { EditQuestionController } from './controllers/edit-question.controller'
 
 @Module({
 	imports: [DatabaseModule, CryptographyModule],
@@ -16,11 +18,13 @@ import { CreateQuestionController } from './controllers/create-question.controll
 		CreateAccountController,
 		AuthenticateController,
 		CreateQuestionController,
+		EditQuestionController,
 	],
 	providers: [
 		RegisterStudentUseCase,
 		AuthenticateStudentUseCase,
 		CreateQuestionUseCase,
+		EditQuestionUseCase,
 	],
 })
 export class HttpModule {}
