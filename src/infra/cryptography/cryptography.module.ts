16ee790ff51b5a6480dfb004f0b149e11c0564e1
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-29 04:06:22 -0300

    ✨ feat: implement JWT authentication module with strategy, guard, and encryption; update environment configuration for JWT keys

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,11 +1,18 @@
+import { Encrypter } from '~/domain/forum/application/cryptography/encrypter'
+import { HashComparer } from '~/domain/forum/application/cryptography/hash-comparer'
 import { HashGenerator } from '~/domain/forum/application/cryptography/hash-generator'
 
 import { Module } from '@nestjs/common'
 
 import { BcryptHasher } from './bcrypt-hasher'
+import { JwtEncrypter } from './jwt-encrypter'
 
 @Module({
-	providers: [{ provide: HashGenerator, useClass: BcryptHasher }],
-	exports: [HashGenerator],
+	providers: [
+		{ provide: Encrypter, useClass: JwtEncrypter },
+		{ provide: HashGenerator, useClass: BcryptHasher },
+		{ provide: HashComparer, useClass: BcryptHasher },
+	],
+	exports: [Encrypter, HashGenerator, HashComparer],
 })
 export class CryptographyModule {}
