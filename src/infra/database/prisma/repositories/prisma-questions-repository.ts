5f2005a5b204b39129150ce56318a9c22e62aebd
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-30 15:07:56 -0300

    ✨ feat: implement pagination for fetching recent questions, including new PaginationParams interface and updates to QuestionsRepository and its implementations

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,4 +1,5 @@
 import { DomainEvents } from '~/core/events/domain-events'
+import { PaginationParams } from '~/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '~/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '~/domain/forum/application/repositories/questions-repository'
 import { Question } from '~/domain/forum/enterprise/entities/question'
@@ -27,6 +28,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
 		}
 
 		return PrismaQuestionMapper.toDomain(question)
+	}
+
+	async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+		const questions = await this.prisma.question.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
+			take: 20,
+			skip: (page - 1) * 20,
+		})
+
+		return questions.map(PrismaQuestionMapper.toDomain)
 	}
 
 	async create(question: Question): Promise<void> {
