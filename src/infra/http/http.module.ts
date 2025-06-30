9deaf945811efed00e9b5f7415c2425cc461c25d
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-30 14:51:37 -0300

    ✨ feat: add DeleteQuestion controller and corresponding end-to-end tests for question deletion, including authorization checks and error handling

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,6 @@
 import { AuthenticateStudentUseCase } from '~/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '~/domain/forum/application/use-cases/create-question'
+import { DeleteQuestionUseCase } from '~/domain/forum/application/use-cases/delete-question'
 import { EditQuestionUseCase } from '~/domain/forum/application/use-cases/edit-question'
 import { RegisterStudentUseCase } from '~/domain/forum/application/use-cases/register-student'
 
@@ -10,6 +11,7 @@ import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 
 @Module({
@@ -19,12 +21,14 @@ import { EditQuestionController } from './controllers/edit-question.controller'
 		AuthenticateController,
 		CreateQuestionController,
 		EditQuestionController,
+		DeleteQuestionController,
 	],
 	providers: [
 		RegisterStudentUseCase,
 		AuthenticateStudentUseCase,
 		CreateQuestionUseCase,
 		EditQuestionUseCase,
+		DeleteQuestionUseCase,
 	],
 })
 export class HttpModule {}
