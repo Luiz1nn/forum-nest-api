e27959f930baebc667f4100dbdb2a2219708ef53
Luis Fernando <luisfernandobr2003@gmail.com>
2025-07-11 18:50:07 -0300

    ✨ feat: implement EditAnswerController and EditAnswer use case; add E2E tests for editing answers and attachments in HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,6 +4,7 @@ import { ChooseQuestionBestAnswerUseCase } from '~/domain/forum/application/use-
 import { CreateQuestionUseCase } from '~/domain/forum/application/use-cases/create-question'
 import { DeleteAnswerUseCase } from '~/domain/forum/application/use-cases/delete-answer'
 import { DeleteQuestionUseCase } from '~/domain/forum/application/use-cases/delete-question'
+import { EditAnswerUseCase } from '~/domain/forum/application/use-cases/edit-answer'
 import { EditQuestionUseCase } from '~/domain/forum/application/use-cases/edit-question'
 import { FetchRecentQuestionsUseCase } from '~/domain/forum/application/use-cases/fetch-recent-questions'
 import { GetQuestionBySlugUseCase } from '~/domain/forum/application/use-cases/get-question-by-slug'
@@ -20,6 +21,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { DeleteAnswerController } from './controllers/delete-answer.controller'
 import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { EditAnswerController } from './controllers/edit-answer.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
@@ -37,6 +39,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
 		AnswerQuestionController,
 		ChooseQuestionBestAnswerController,
 		DeleteAnswerController,
+		EditAnswerController,
 	],
 	providers: [
 		RegisterStudentUseCase,
@@ -49,6 +52,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
 		AnswerQuestionUseCase,
 		ChooseQuestionBestAnswerUseCase,
 		DeleteAnswerUseCase,
+		EditAnswerUseCase,
 	],
 })
 export class HttpModule {}
