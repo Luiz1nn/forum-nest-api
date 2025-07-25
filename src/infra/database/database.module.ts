f115cec091d1078f29b01c5690b0b99c1b1448b8
Luiz1nn <luisfernandobr2003@gmail.com>
2025-07-25 13:11:19 -0300

    ✨ feat: add Redis caching support with CacheModule and RedisCacheRepository implementation

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -10,6 +10,7 @@ import { NotificationsRepository } from '~/domain/notification/application/repos
 
 import { Module } from '@nestjs/common'
 
+import { CacheModule } from '../cache/cache.module'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
@@ -22,6 +23,7 @@ import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question
 import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
+	imports: [CacheModule],
 	providers: [
 		PrismaService,
 		{
