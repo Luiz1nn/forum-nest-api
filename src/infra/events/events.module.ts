838285ea6458816a6589c978f1518dc691cd23e9
Luiz1nn <luisfernandobr2003@gmail.com>
2025-07-25 13:17:45 -0300

    ✨ feat: add EventsModule to handle notification events and integrate SendNotificationUseCase

---
+++ src/infra/events/events.module.ts
@@ -0,0 +1,11 @@
+import { SendNotificationUseCase } from '~/domain/notification/application/use-cases/send-notification'
+
+import { Module } from '@nestjs/common'
+
+import { DatabaseModule } from '../database/database.module'
+
+@Module({
+	imports: [DatabaseModule],
+	providers: [SendNotificationUseCase],
+})
+export class EventsModule {}
